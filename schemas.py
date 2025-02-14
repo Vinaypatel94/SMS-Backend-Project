f41522ad240d8ae70cfe27ba300deff069c55399
vinay patel <vinaypate94@gmail.com>
2025-02-14 01:57:30 +0000

    update file

--- schemas.py
+++ schemas.py
@@ -1,26 +1,17 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
+from typing import Optional, List
 
-class UserCreate(BaseModel):
-    name: str
-    age: int
-    email: EmailStr
-    phone_no:int
-    username: str
-    password: str
-    role: Optional[str] = "staff"
-    
-    
+
 class UserResponse(BaseModel):
     id: int
     name: str
     age: int
     phone_no: int
     email: str
-    username: str 
+    username: str
     role: str
-    
-    
+
+
 class UserLogin(BaseModel):
     username: str
     password: str
@@ -30,4 +21,24 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
-# Attendance schemas
+
+class PermissionSchema(BaseModel):
+    id: Optional [int]
+    name: str
+
+
+class RoleSchema(BaseModel):
+    id: Optional[int]
+    name: str
+    permissions: List[PermissionSchema] = []
+
+
+class UserCreate(BaseModel):
+    id: Optional[int]
+    name: str
+    age: int
+    email: EmailStr
+    phone_no: int
+    username: str
+    password: str
+    roles: List[str] = []
